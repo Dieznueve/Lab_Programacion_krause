971d706434521e0102930bc82079de353f9bb7be
Dieznueve <Llanothiagonahuel@gmail.com>
2026-05-29 22:37:39 -0300

    Add files via upload

---
+++ Curso.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+
+public class Curso {
+    private String nombre;
+    private ArrayList<Estudiante> alumnos;
+
+    public Curso(String nombre) {
+        this.nombre = nombre;
+        this.alumnos = new ArrayList<>();
+    }
+
+    public void inscribir(Estudiante e) {
+        alumnos.add(e);
+    }
+
+    public void darDeBaja(int indice) {
+        alumnos.remove(indice);
+    }
+
+    public void listarAlumnos() {
+        System.out.println("Alumnos inscritos en el curso " + nombre + ":");
+        for (Estudiante e : alumnos) {
+            e.mostrarFicha();
+        }
+    }
+
+    public int cantidadAlumnos() {
+        return alumnos.size();
+    }
+}
